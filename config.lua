1a91b0faf3f8d631d9267c6824054b1d00f7c301
nekot <34742471+aposimz@users.noreply.github.com>
2026-01-11 22:33:47 +0900

    Add files via upload

--- config.lua
+++ config.lua
@@ -4,11 +4,19 @@ Config.vehiclespeedlimiter = true -- 速度制限するか
 
 -- m/s 指定（例: 200km/h = 200 / 3.6 = 55.56）
 -- ※必ず小数点付きで指定してください（例: 84.00）。もしくは「km/h / 3.6」の式でも可
-Config.speedlimit = 220 / 3.6 -- 一般車両の上限
-Config.emergencyspeedlimit = 270 / 3.6 -- 緊急車両(18)用の上限
+Config.speedlimit = 221 / 3.6 -- 一般車両の上限
+Config.emergencyspeedlimit = 271 / 3.6 -- 緊急車両(18)用の上限
 
 Config.vehicleweapons = false -- false で車両武器（ドライブバイ等）を無効化
 Config.debug = false -- デバッグログの出力を有効化
+
+-- 犯罪利用禁止 判定の設定
+-- 1) 車両モデル指定：モデル名（スポーン名）
+Config.prohibitModelNames = { "annihilator", "tug", "raidengrb" }
+
+-- 2) ナンバープレートの部分一致ワード。例: { "GANG", "TEST" }
+--    大文字/小文字は無視（部分一致）
+Config.prohibitPlateWords = { "WCAT", "XXX" }
 
 -- ヘリ(15)・飛行機(16)は対象外
 --[[ 車両クラス一覧
